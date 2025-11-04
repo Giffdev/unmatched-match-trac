58c4468cd462cd3feef89113d4eb0b00eafd6841
Devin Sinha <devsin@microsoft.com>
2025-11-04 22:35:33 +0000

    Generated by Spark: your data for sets is wrong. here is the data as I know it:
    Here’s a comprehensive list of Unmatched box sets and the heroes included in each:
    
    📦 Official Unmatched Sets
    1. Battle of Legends, Volume One (Sept 2019)
    Heroes: Medusa, Sinbad, Alice, King Arthur
    unmatched
    2. Bruce Lee (Oct 2019) (Standalone Expansion)
    Hero: Bruce Lee
    unmatched
    3. Robin Hood vs. Bigfoot (Oct 2019)
    Heroes: Robin Hood, Bigfoot
    unmatched
    4. Jurassic Park – InGen vs. Raptors (Apr 2020)
    Heroes: InGen, Raptors
    unmatched
    5. Cobble & Fog (July 2020)
    Heroes: Sherlock Holmes, Dracula, Jekyll & Hyde, Invisible Man
    unmatched
    6. Buffy the Vampire Slayer (Oct 2020)
    Heroes: Buffy, Angel, Spike, Willow
    unmatched
    7. Little Red Riding Hood vs. Beowulf (Dec 2020)
    Heroes: Little Red Riding Hood, Beowulf
    unmatched
    8. Deadpool (July 2021) (Standalone Expansion)
    Hero: Deadpool
    unmatched
    9. Battle of Legends, Volume Two (Dec 2021)
    Heroes: Achilles, Bloody Mary, Sun Wukong, Yennenga
    unmatched
    10. Redemption Row (Mar 2022)
    Heroes: Luke Cage, Moon Knight, Ghost Rider
    unmatched
    11. Hell’s Kitchen (Mar 2022)
    Heroes: Daredevil, Bullseye, Elektra
    unmatched
    12. Jurassic Park – Sattler vs. T‑Rex (Apr 2022)
    Heroes: Dr. Ellie Sattler, T‑Rex
    unmatched
    13. Houdini vs. The Genie (Dec 2022)
    Heroes: Harry Houdini, The Genie
    unmatched
    14. Teen Spirit (Mar 2023)
    Heroes: Ms. Marvel, Squirrel Girl, Cloak & Dagger
    unmatched
    15. For King and Country (Apr 2023)
    Heroes: Black Widow, Black Panther, Winter Soldier
    unmatched
    16. Brains & Brawn (June 2023)
    Heroes: Spider‑Man, She‑Hulk, Doctor Strange
    unmatched
    17. Unmatched Adventures – Tales to Amaze (Aug 2023) (Co-op)
    Heroes: Nikola Tesla, Dr. Jill Trent, Golden Bat, Annie Christmas
    unmatched
    18. Sun’s Origin (Jan 2024) (Design Contest)
    Heroes: Tomoe Gozen, Oda Nobunaga
    unmatched
    19. Slings & Arrows (May 2024) (Design Contest)
    Heroes: Hamlet, Titania, The Wayward Sisters, William Shakespeare
    unmatched
    20. Realm’s Fall (Nov 2024) (The Witcher)
    Heroes: Eredin, Philippa, Yennefer & Triss
    unmatched
    21. Steel & Silver (Nov 2024) (The Witcher)
    Heroes: Geralt of Rivia, Ciri, Ancient Leshen
    unmatched
    22. Muhammad Ali vs. Bruce Lee (Date TBD)
    Heroes: Muhammad Ali, Bruce Lee (reprint + new figures/maps)
    unmatched
    🧩 Additional Expansions & Contest Packs
    Shredder & Krang Hero Decks (TMNT Villains)
    unmatched
    Unmatched Adventures – Teenage Mutant Ninja Turtles (TMNT Heroes & Minions)
    unmatched

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,149 +1,153 @@
 import type { Hero, Map } from './types'
 
 export const UNMATCHED_SETS = [
-  'Battle of Legends Volume 1',
-  'Battle of Legends Volume 2',
-  'Cobble & Fog',
-  'Robin Hood vs Bigfoot',
+  'Battle of Legends, Volume One',
   'Bruce Lee',
-  'Little Red Riding Hood vs Beowulf',
-  'Jurassic Park - Dr. Sattler vs T. Rex',
-  'Jurassic Park - InGen vs Raptors',
+  'Robin Hood vs. Bigfoot',
+  'Jurassic Park – InGen vs. Raptors',
+  'Cobble & Fog',
   'Buffy the Vampire Slayer',
+  'Little Red Riding Hood vs. Beowulf',
   'Deadpool',
-  'Teen Spirit',
-  'Houdini vs The Genie',
-  'For King and Country',
-  'Steel and Silver',
-  'Sun Wukong',
+  'Battle of Legends, Volume Two',
   'Redemption Row',
-  'Brains and Brawn',
   'Hell\'s Kitchen',
+  'Jurassic Park – Sattler vs. T‑Rex',
+  'Houdini vs. The Genie',
+  'Teen Spirit',
+  'For King and Country',
+  'Brains & Brawn',
+  'Unmatched Adventures – Tales to Amaze',
+  'Sun\'s Origin',
   'Slings & Arrows',
-  'Bloody Mary vs The Headless Horseman',
+  'Realm\'s Fall',
+  'Steel & Silver',
+  'Muhammad Ali vs. Bruce Lee',
 ]
 
 export const HEROES: Hero[] = [
-  { id: 'alice', name: 'Alice', set: 'Battle of Legends Volume 1' },
-  { id: 'medusa', name: 'Medusa', set: 'Battle of Legends Volume 1' },
-  { id: 'king-arthur', name: 'King Arthur', set: 'Battle of Legends Volume 1' },
-  { id: 'sinbad', name: 'Sinbad', set: 'Battle of Legends Volume 1' },
-  
-  { id: 'achilles', name: 'Achilles', set: 'Battle of Legends Volume 2' },
-  { id: 'sun-wukong-vol2', name: 'Sun Wukong', set: 'Battle of Legends Volume 2' },
-  { id: 'bloody-mary-vol2', name: 'Bloody Mary', set: 'Battle of Legends Volume 2' },
-  { id: 'yennenga', name: 'Yennenga', set: 'Battle of Legends Volume 2' },
-  
-  { id: 'invisible-man', name: 'Invisible Man', set: 'Cobble & Fog' },
-  { id: 'sherlock-holmes', name: 'Sherlock Holmes', set: 'Cobble & Fog' },
-  { id: 'dracula', name: 'Dracula', set: 'Cobble & Fog' },
-  { id: 'jekyll-hyde', name: 'Jekyll & Hyde', set: 'Cobble & Fog' },
-  
-  { id: 'robin-hood', name: 'Robin Hood', set: 'Robin Hood vs Bigfoot' },
-  { id: 'bigfoot', name: 'Bigfoot', set: 'Robin Hood vs Bigfoot' },
+  { id: 'alice', name: 'Alice', set: 'Battle of Legends, Volume One' },
+  { id: 'medusa', name: 'Medusa', set: 'Battle of Legends, Volume One' },
+  { id: 'king-arthur', name: 'King Arthur', set: 'Battle of Legends, Volume One' },
+  { id: 'sinbad', name: 'Sinbad', set: 'Battle of Legends, Volume One' },
   
   { id: 'bruce-lee', name: 'Bruce Lee', set: 'Bruce Lee' },
   
-  { id: 'little-red', name: 'Little Red Riding Hood', set: 'Little Red Riding Hood vs Beowulf' },
-  { id: 'beowulf', name: 'Beowulf', set: 'Little Red Riding Hood vs Beowulf' },
+  { id: 'robin-hood', name: 'Robin Hood', set: 'Robin Hood vs. Bigfoot' },
+  { id: 'bigfoot', name: 'Bigfoot', set: 'Robin Hood vs. Bigfoot' },
   
-  { id: 't-rex', name: 'T. Rex', set: 'Jurassic Park - Dr. Sattler vs T. Rex' },
-  { id: 'ellie-sattler', name: 'Dr. Ellie Sattler', set: 'Jurassic Park - Dr. Sattler vs T. Rex' },
+  { id: 'ingen', name: 'InGen', set: 'Jurassic Park – InGen vs. Raptors' },
+  { id: 'raptors', name: 'Raptors', set: 'Jurassic Park – InGen vs. Raptors' },
   
-  { id: 'robert-muldoon', name: 'Robert Muldoon', set: 'Jurassic Park - InGen vs Raptors' },
-  { id: 'raptors', name: 'Raptors', set: 'Jurassic Park - InGen vs Raptors' },
+  { id: 'sherlock-holmes', name: 'Sherlock Holmes', set: 'Cobble & Fog' },
+  { id: 'dracula', name: 'Dracula', set: 'Cobble & Fog' },
+  { id: 'jekyll-hyde', name: 'Jekyll & Hyde', set: 'Cobble & Fog' },
+  { id: 'invisible-man', name: 'Invisible Man', set: 'Cobble & Fog' },
   
   { id: 'buffy', name: 'Buffy', set: 'Buffy the Vampire Slayer' },
   { id: 'angel', name: 'Angel', set: 'Buffy the Vampire Slayer' },
   { id: 'spike', name: 'Spike', set: 'Buffy the Vampire Slayer' },
   { id: 'willow', name: 'Willow', set: 'Buffy the Vampire Slayer' },
   
+  { id: 'little-red', name: 'Little Red Riding Hood', set: 'Little Red Riding Hood vs. Beowulf' },
+  { id: 'beowulf', name: 'Beowulf', set: 'Little Red Riding Hood vs. Beowulf' },
+  
   { id: 'deadpool', name: 'Deadpool', set: 'Deadpool' },
   
-  { id: 'buffy-teen', name: 'Buffy', set: 'Teen Spirit' },
-  { id: 'angel-teen', name: 'Angel', set: 'Teen Spirit' },
-  { id: 'spike-teen', name: 'Spike', set: 'Teen Spirit' },
-  { id: 'willow-teen', name: 'Willow', set: 'Teen Spirit' },
-  { id: 'oz', name: 'Oz', set: 'Teen Spirit' },
-  { id: 'faith', name: 'Faith', set: 'Teen Spirit' },
+  { id: 'achilles', name: 'Achilles', set: 'Battle of Legends, Volume Two' },
+  { id: 'bloody-mary', name: 'Bloody Mary', set: 'Battle of Legends, Volume Two' },
+  { id: 'sun-wukong', name: 'Sun Wukong', set: 'Battle of Legends, Volume Two' },
+  { id: 'yennenga', name: 'Yennenga', set: 'Battle of Legends, Volume Two' },
   
-  { id: 'houdini', name: 'Houdini', set: 'Houdini vs The Genie' },
-  { id: 'the-genie', name: 'The Genie', set: 'Houdini vs The Genie' },
-  
-  { id: 'robin-hood-fkac', name: 'Robin Hood', set: 'For King and Country' },
-  { id: 'richard-lionheart', name: 'Richard the Lionheart', set: 'For King and Country' },
-  { id: 'king-arthur-fkac', name: 'King Arthur', set: 'For King and Country' },
-  
-  { id: 'geralt', name: 'Geralt of Rivia', set: 'Steel and Silver' },
-  { id: 'ciri', name: 'Ciri', set: 'Steel and Silver' },
-  { id: 'yennefer', name: 'Yennefer', set: 'Steel and Silver' },
-  
-  { id: 'sun-wukong', name: 'Sun Wukong', set: 'Sun Wukong' },
-  
-  { id: 'moon-knight', name: 'Moon Knight', set: 'Redemption Row' },
   { id: 'luke-cage', name: 'Luke Cage', set: 'Redemption Row' },
+  { id: 'moon-knight', name: 'Moon Knight', set: 'Redemption Row' },
   { id: 'ghost-rider', name: 'Ghost Rider', set: 'Redemption Row' },
   
-  { id: 'black-panther', name: 'Black Panther', set: 'Brains and Brawn' },
-  { id: 'shuri', name: 'Shuri', set: 'Brains and Brawn' },
-  { id: 'she-hulk', name: 'She-Hulk', set: 'Brains and Brawn' },
-  
   { id: 'daredevil', name: 'Daredevil', set: 'Hell\'s Kitchen' },
-  { id: 'elektra', name: 'Elektra', set: 'Hell\'s Kitchen' },
   { id: 'bullseye', name: 'Bullseye', set: 'Hell\'s Kitchen' },
+  { id: 'elektra', name: 'Elektra', set: 'Hell\'s Kitchen' },
   
-  { id: 'bloody-mary', name: 'Bloody Mary', set: 'Bloody Mary vs The Headless Horseman' },
-  { id: 'headless-horseman', name: 'The Headless Horseman', set: 'Bloody Mary vs The Headless Horseman' },
+  { id: 'ellie-sattler', name: 'Dr. Ellie Sattler', set: 'Jurassic Park – Sattler vs. T‑Rex' },
+  { id: 't-rex', name: 'T‑Rex', set: 'Jurassic Park – Sattler vs. T‑Rex' },
   
-  { id: 'clint-barton', name: 'Clint Barton', set: 'Slings & Arrows' },
-  { id: 'kate-bishop', name: 'Kate Bishop', set: 'Slings & Arrows' },
+  { id: 'houdini', name: 'Harry Houdini', set: 'Houdini vs. The Genie' },
+  { id: 'the-genie', name: 'The Genie', set: 'Houdini vs. The Genie' },
+  
+  { id: 'ms-marvel', name: 'Ms. Marvel', set: 'Teen Spirit' },
+  { id: 'squirrel-girl', name: 'Squirrel Girl', set: 'Teen Spirit' },
+  { id: 'cloak-dagger', name: 'Cloak & Dagger', set: 'Teen Spirit' },
+  
+  { id: 'black-widow', name: 'Black Widow', set: 'For King and Country' },
+  { id: 'black-panther', name: 'Black Panther', set: 'For King and Country' },
+  { id: 'winter-soldier', name: 'Winter Soldier', set: 'For King and Country' },
+  
+  { id: 'spider-man', name: 'Spider‑Man', set: 'Brains & Brawn' },
+  { id: 'she-hulk', name: 'She‑Hulk', set: 'Brains & Brawn' },
+  { id: 'doctor-strange', name: 'Doctor Strange', set: 'Brains & Brawn' },
+  
+  { id: 'nikola-tesla', name: 'Nikola Tesla', set: 'Unmatched Adventures – Tales to Amaze' },
+  { id: 'dr-jill-trent', name: 'Dr. Jill Trent', set: 'Unmatched Adventures – Tales to Amaze' },
+  { id: 'golden-bat', name: 'Golden Bat', set: 'Unmatched Adventures – Tales to Amaze' },
+  { id: 'annie-christmas', name: 'Annie Christmas', set: 'Unmatched Adventures – Tales to Amaze' },
+  
+  { id: 'tomoe-gozen', name: 'Tomoe Gozen', set: 'Sun\'s Origin' },
+  { id: 'oda-nobunaga', name: 'Oda Nobunaga', set: 'Sun\'s Origin' },
+  
+  { id: 'hamlet', name: 'Hamlet', set: 'Slings & Arrows' },
+  { id: 'titania', name: 'Titania', set: 'Slings & Arrows' },
+  { id: 'wayward-sisters', name: 'The Wayward Sisters', set: 'Slings & Arrows' },
+  { id: 'william-shakespeare', name: 'William Shakespeare', set: 'Slings & Arrows' },
+  
+  { id: 'eredin', name: 'Eredin', set: 'Realm\'s Fall' },
+  { id: 'philippa', name: 'Philippa', set: 'Realm\'s Fall' },
+  { id: 'yennefer-triss', name: 'Yennefer & Triss', set: 'Realm\'s Fall' },
+  
+  { id: 'geralt', name: 'Geralt of Rivia', set: 'Steel & Silver' },
+  { id: 'ciri', name: 'Ciri', set: 'Steel & Silver' },
+  { id: 'ancient-leshen', name: 'Ancient Leshen', set: 'Steel & Silver' },
+  
+  { id: 'muhammad-ali', name: 'Muhammad Ali', set: 'Muhammad Ali vs. Bruce Lee' },
+  { id: 'bruce-lee-ali', name: 'Bruce Lee', set: 'Muhammad Ali vs. Bruce Lee' },
 ]
 
 export const MAPS: Map[] = [
-  { id: 'sarpedon', name: 'Sarpedon', set: 'Battle of Legends Volume 1' },
-  { id: 'mariner-bay', name: 'Mariner Bay', set: 'Battle of Legends Volume 1' },
+  { id: 'sarpedon', name: 'Sarpedon', set: 'Battle of Legends, Volume One' },
+  { id: 'mariner-bay', name: 'Mariner Bay', set: 'Battle of Legends, Volume One' },
   
-  { id: 'heorot', name: 'Heorot', set: 'Battle of Legends Volume 2' },
-  { id: 'hanging-gardens', name: 'Hanging Gardens', set: 'Battle of Legends Volume 2' },
+  { id: 'insula-dulcamara', name: 'Insula Dulcamara', set: 'Bruce Lee' },
+  
+  { id: 'sherwood-forest', name: 'Sherwood Forest', set: 'Robin Hood vs. Bigfoot' },
+  { id: 'yukon', name: 'Yukon', set: 'Robin Hood vs. Bigfoot' },
+  
+  { id: 'raptor-paddock', name: 'Raptor Paddock', set: 'Jurassic Park – InGen vs. Raptors' },
   
   { id: 'baskerville-manor', name: 'Baskerville Manor', set: 'Cobble & Fog' },
   { id: 'soho', name: 'SOHO', set: 'Cobble & Fog' },
   
-  { id: 'sherwood-forest', name: 'Sherwood Forest', set: 'Robin Hood vs Bigfoot' },
-  { id: 'yukon', name: 'Yukon', set: 'Robin Hood vs Bigfoot' },
-  
-  { id: 'insula-dulcamara', name: 'Insula Dulcamara', set: 'Bruce Lee' },
-  
-  { id: 'heorot-lrrh', name: 'Heorot', set: 'Little Red Riding Hood vs Beowulf' },
-  
-  { id: 'jurassic-park', name: 'Jurassic Park', set: 'Jurassic Park - Dr. Sattler vs T. Rex' },
-  
-  { id: 'raptor-paddock', name: 'Raptor Paddock', set: 'Jurassic Park - InGen vs Raptors' },
-  
   { id: 'sunnydale-high', name: 'Sunnydale High', set: 'Buffy the Vampire Slayer' },
   
+  { id: 'heorot', name: 'Heorot', set: 'Little Red Riding Hood vs. Beowulf' },
+  
   { id: 'ny-1901', name: 'New York 1901', set: 'Deadpool' },
+  
+  { id: 'hanging-gardens', name: 'Hanging Gardens', set: 'Battle of Legends, Volume Two' },
+  
+  { id: 'hells-kitchen', name: 'Hell\'s Kitchen', set: 'Hell\'s Kitchen' },
+  
+  { id: 'sanctum-sanctorum', name: 'Sanctum Sanctorum', set: 'Redemption Row' },
+  
+  { id: 'jurassic-park', name: 'Jurassic Park', set: 'Jurassic Park – Sattler vs. T‑Rex' },
+  
+  { id: 'king-solomons-mine', name: 'King Solomon\'s Mine', set: 'Houdini vs. The Genie' },
   
   { id: 'library', name: 'The Library', set: 'Teen Spirit' },
   { id: 'the-bronze', name: 'The Bronze', set: 'Teen Spirit' },
   
-  { id: 'king-solomons-mine', name: 'King Solomon\'s Mine', set: 'Houdini vs The Genie' },
+  { id: 'helicarrier', name: 'The Helicarrier', set: 'For King and Country' },
   
-  { id: 'sherwood-forest-fkac', name: 'Sherwood Forest', set: 'For King and Country' },
+  { id: 'wakanda', name: 'Wakanda', set: 'Brains & Brawn' },
   
-  { id: 'kaer-morhen', name: 'Kaer Morhen', set: 'Steel and Silver' },
-  
-  { id: 'great-wall', name: 'The Great Wall', set: 'Sun Wukong' },
-  
-  { id: 'hells-kitchen', name: 'Hell\'s Kitchen', set: 'Hell\'s Kitchen' },
-  
-  { id: 'navy-pier', name: 'Navy Pier', set: 'Bloody Mary vs The Headless Horseman' },
-  
-  { id: 'sanctum-sanctorum', name: 'Sanctum Sanctorum', set: 'Redemption Row' },
-  
-  { id: 'wakanda', name: 'Wakanda', set: 'Brains and Brawn' },
-  
-  { id: 'helicarrier', name: 'The Helicarrier', set: 'Slings & Arrows' },
+  { id: 'kaer-morhen', name: 'Kaer Morhen', set: 'Steel & Silver' },
 ]
 
 export function getHeroById(id: string): Hero | undefined {
